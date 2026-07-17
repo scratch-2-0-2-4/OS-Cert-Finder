2a0516bfac2d51dd89d02df6a76c6bf8b89eca61
Scratch_2_0_2_4 <scratch.20.24.0@gmail.com>
2026-07-17 15:11:04 +0200

    Update ADMIN.py

--- ADMIN.py
+++ ADMIN.py
@@ -1,9 +1,19 @@
 import re
+import os
 
 pattern = r"^[A-Z]{3}-\d{3}-[a-z]{3}$"
 admin = input("Certificat >>> ")
 
+MDP_LHF = os.getenv('MDP_LHF_VVM')
+
 if re.fullmatch(pattern, admin):
-  pass
-else :
-  pass
+    if admin == "LHF-557-vvm":
+        mdp = input("Mot de Passe ? >>> ")
+        if MDP_LHF == mdp:
+            print("Ok")
+        else:
+            pass
+    else:
+        pass
+else:
+    pass
