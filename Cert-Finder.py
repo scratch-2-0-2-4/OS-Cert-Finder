e4e911189125f2dc9f24912f06f711918bd17bae
Scratch_2_0_2_4 <scratch.20.24.0@gmail.com>
2026-07-17 15:00:57 +0200

    Update and rename script.py to Cert-Finder.py

--- Cert-Finder.py
+++ Cert-Finder.py
@@ -12,26 +12,15 @@ if re.fullmatch(r"^[A-Z]{3}-\d{3}-[a-z]{3}$", Certificat):
     print(f"Certificat n° \033[1m{Certificat}\033[0m \033[32m ✔ Ok\033[0m")
     print("Créateur : \033[1m Scratch_2_0_2_4\033[0m \033[34m vérifié \033[0m")
     print("Nom de l'OS : \033[1m Scratch.OS X\033[m")
-
   elif Certificat == "ABC-123-def":
      print(f"Certificat n° \033[1m{Certificat}\033[0m \033[33m certificat exemple \033[0m")
-
   else :
      print(f"\033[31m ✖ Le certificat \033[1m{Certificat}\033[m \033[31m n'existe pas.\033[0m")
-
-
 elif Certificat == "ADMIN" :
   print("ADMIN Mode")
   ADMIN = input("Administration >>> ")
-
-  if ADMIN == "LHF-557-vvm" :
-     Mdp-LHF-vvm = os.getenv('MDP_LHF_VVM')
-
-
 elif Certificat == "help" :
   print("Commandes : ")
   print("• Recherche de certificats")
-
-
 else:
     print("\033[31m ✖ Format invalide. Format correct : \033[1m ABC-123-def \033[0m \033[0m")
