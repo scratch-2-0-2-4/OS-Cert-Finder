35c34120756518bbbbfba79588ffb826eec57a77
Scratch_2_0_2_4 <scratch.20.24.0@gmail.com>
2026-07-23 14:55:47 +0200

    Update --version.py

--- --version.py
+++ --version.py
@@ -1,4 +1,4 @@
 v = 2.8
-print(f"""\n Version actuelle : [v. {v}].\n \n
-Rendez-vous sur :\n
-https://github.com/Scratch-OS-X/OS-Cert-Center/wiki/OS-Certificate-Center-(logiciel)-%E2%80%90-version pour vérifier votre version.""")
+print(f"""\n Version actuelle : [v. {v}].\n
+Pour vérifier votre version, rendez-vous sur :\n
+https://github.com/Scratch-OS-X/OS-Cert-Center/wiki/OS-Certificate-Center-(logiciel)-%E2%80%90-version \n""")
